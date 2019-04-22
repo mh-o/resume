de17e9cf3891166dd8a556d0f0578865e9d4a623
Michael Okonski <mho8@pitt.edu>
2019-04-22 17:14:27 -0400

    setup

---
+++ Google's Kick Start 2013/Practice Round/BadHorse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class BadHorse {
+  public static void main(String[] args) {
+    // To read from input file.
+    Scanner in = new Scanner(new BufferedReader(
+      new InputStreamReader(System.in)));
+
+    num_test_cases = in.nextInt();
+
+    // Iterate through all test cases.
+    for (int i = 0; i < num_test_cases; i++) {
+      testCase()
+    }
+  }
+}
