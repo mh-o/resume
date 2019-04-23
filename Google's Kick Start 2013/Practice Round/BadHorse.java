b5fefff7fa578326bf24beff989d7bfd53339a92
mh-o <michael.okonski@gmail.com>
2019-04-23 15:17:39 -0700

    update

--- Google's Kick Start 2013/Practice Round/BadHorse.java
+++ Google's Kick Start 2013/Practice Round/BadHorse.java
@@ -1,17 +1,28 @@
+/*
+ * Compile: `javac BadHorse.java`
+ * Execute: `java BadHorse < infile`
+ * Execute (powershell): `Get-Content infile | java BadHorse`
+ */
+
 import java.util.*;
 import java.io.*;
 
 public class BadHorse {
   public static void main(String[] args) {
-    // To read from input file.
+    // To read input.
     Scanner in = new Scanner(new BufferedReader(
       new InputStreamReader(System.in)));
 
-    num_test_cases = in.nextInt();
+    int num_test_cases = in.nextInt();
 
     // Iterate through all test cases.
     for (int i = 0; i < num_test_cases; i++) {
-      testCase()
+      int num_pairs = in.nextInt();
+      testCase(num_pairs);
     }
+  }
+
+  private static void testCase(int num_pairs) {
+
   }
 }
