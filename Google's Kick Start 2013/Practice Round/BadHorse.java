6dfa1a1c162b1446ff5670d827114e20ac9bd71d
mh-o <michael.okonski@gmail.com>
2019-04-24 12:19:02 -0700

    ready to start the hard part

--- Google's Kick Start 2013/Practice Round/BadHorse.java
+++ Google's Kick Start 2013/Practice Round/BadHorse.java
@@ -7,11 +7,17 @@
 import java.util.*;
 import java.io.*;
 
+/*
+ * Class for the Bad Horse problem from the practice round of Google's 2013
+ * Kick Start coding challenge.
+ */
 public class BadHorse {
-
   static Scanner in;
   static ArrayList<Member> members;
 
+  /*
+   * Main program.
+   */
   public static void main(String[] args) {
     // To read input.
     in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
@@ -30,6 +36,10 @@ public class BadHorse {
     printMembers();
   }
 
+  /*
+   * Method for a single test case, takes all troublesome pairs and decides if
+   * they case be split into two groups of completely disconnected graphs.
+   */
   private static void testCase(int num_pairs) {
     // Iterate through all pairs for a single test case.
     for (int i = 0; i < num_pairs; i++) {
@@ -41,30 +51,67 @@ public class BadHorse {
     }
   }
 
+  /*
+   * Method to add a member to the members list if they are not already present
+   * in the list. Will also update the member object to add the enemy for that
+   * member.
+   */
   private static void addMember(String member, String enemy) {
-    if (!searchMembers(member)) {
+    int index = searchMembers(member);
+
+    if (index < 0) {
       // Create our new member since we couldn't find them in our list.
       Member new_member = new Member(member);
 
       // Add the enemy if they don't exist yet and then add our new member.
       new_member.addEnemy(enemy);
       members.add(new_member);
+    } else {
+      members.get(index).addEnemy(enemy);
     }
   }
 
-  private static Boolean searchMembers(String str) {
+  /*
+   * Helper method to search through all members in a test case to see if they
+   * exist within the list.
+   *
+   * @param string of member to search
+   * @return index of member if they exist, -1 otherwise
+   */
+  private static int searchMembers(String str) {
     for (int i = 0; i < members.size(); i++) {
       if (members.get(i).getName().equals(str)) {
-        return true;
+        return i;
       }
     }
 
-    return false;
+    return -1;
   }
 
+  /*
+   * Helper method to print all unique members for a test case.
+   */
   private static void printMembers() {
+    System.out.println();
+
+    // Iterate through all unique members in the order we found them.
     for (int i = 0; i < members.size(); i++) {
-      System.out.println(members.get(i).getName());
+      System.out.print(members.get(i).getName() + ": ");
+
+      String[] names = members.get(i).getEnemies();
+
+      // Iterate through each enemy of the current member.
+      for (int j = 0; j < names.length; j++) {
+        System.out.print(names[j]);
+
+        if (j != (names.length - 1)) {
+          System.out.print(", ");
+        } else {
+          System.out.println();
+        }
+      }
     }
+
+    System.out.println();
   }
 }
--- Google's Kick Start 2013/Practice Round/Member.java
+++ Google's Kick Start 2013/Practice Round/Member.java
@@ -1,31 +1,64 @@
 import java.util.*;
 
+/*
+ * Class for a member object, which stores the members own name as well as a
+ * list of enemies (or nodes) that the member should not be in a gruop (graph)
+ * with.
+ */
 public class Member {
     private String name;
     private ArrayList<String> enemies;
 
+    /*
+     * Constructor, sets the name of the new member and initializes an empty
+     * list of enemies.
+     */
     Member(String str) {
       name = str;
       enemies = new ArrayList<String>();
     }
 
+    /*
+     * Method to add an enemy to this member's list of enemies - if that enemy
+     * is not already listed.
+     */
     public void addEnemy(String str) {
       if (!searchEnemies(str)) {
         enemies.add(str);
       }
     }
 
+    /*
+     * Method to search through the list of enemies and see if one exists.
+     */
     public Boolean searchEnemies(String str) {
       for (int i = 0; i < enemies.size(); i++) {
         if (enemies.get(i).equals(str)) {
-          return false;
+          return true;
         }
       }
 
-      return true;
+      return false;
     }
 
+    /*
+     * Method to return this member's name as a string.
+     */
     public String getName() {
       return name;
+    }
+
+    /*
+     * Method to return an array of strings with the name of each enemy for this
+     * member.
+     */
+    public String[] getEnemies() {
+      String[] names = new String[enemies.size()];
+
+      for (int i = 0; i < enemies.size(); i++) {
+        names[i] = enemies.get(i);
+      }
+
+      return names;
     }
 }
