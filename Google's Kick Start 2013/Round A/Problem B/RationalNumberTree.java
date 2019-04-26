f93688191b9853dc095b8c98287d3c9bbf0992c7
mh-o <michael.okonski@gmail.com>
2019-04-26 12:18:54 -0400

    works, but only for values within Java's int range

--- Google's Kick Start 2013/Round A/Problem B/RationalNumberTree.java
+++ Google's Kick Start 2013/Round A/Problem B/RationalNumberTree.java
@@ -1,8 +1,8 @@
 /*
  * Compile: `javac RationalNumberTree.java`
  * Execute: `java RationalNumberTree < infile > outfile`
- * Execute (powershell): `Get-Content infile | java RationalNumberTree |
- *                        Out-File -Filepath outfile -Encoding ASCII`
+ * Execute (powershell):
+ * > Get-Content infile | java RationalNumberTree | Out-File -Filepath outfile -Encoding ASCII
  */
 
 import java.util.*;
@@ -10,6 +10,7 @@ import java.io.*;
 
 public class RationalNumberTree {
   static Scanner in;
+  static int[] frac = { 0, 1 };
 
   public static void main(String[] args) {
     in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
@@ -18,8 +19,8 @@ public class RationalNumberTree {
     in.nextLine();
 
     // Iterate through all test cases.
-    for (int i = 0; i < 4; i++) {
-      String res = "Case #" + i + ": ";
+    for (int i = 0; i < num_test_cases; i++) {
+      String res = "Case #" + (i + 1) + ": ";
       res += testCase(in.nextLine());
 
       System.out.println(res);
@@ -29,6 +30,8 @@ public class RationalNumberTree {
   private static String testCase(String str) {
     String[] split = str.split(" ");
     String res = "";
+    frac[0] = 0;
+    frac[1] = 1;
 
     if (split[0].equals("1")) {
       int[] val = findNthElement(Integer.parseInt(split[1]));
@@ -36,15 +39,19 @@ public class RationalNumberTree {
       res += Integer.toString(val[0]);
       res += " ";
       res += Integer.toString(val[1]);
-    } else if (split[1].equals("2")) {
-      findPosition(Integer.parseInt(split[1]), Integer.parseInt(split[2]));
+    } else if (split[0].equals("2")) {
+      int val = findPosition(Integer.parseInt(split[1]),
+                             Integer.parseInt(split[2]));
+
+      res += Integer.toString(val);
     }
 
     return res;
   }
 
-  static int[] frac = { 0, 1 };
-
+/*
+ * Method to
+ */
   private static int[] findNthElement(int n) {
     if (n > 0) {
       findNthElement(n / 2);
@@ -55,7 +62,60 @@ public class RationalNumberTree {
     return frac;
   }
 
-  private static void findPosition(int p, int q) {
+  private static int findPosition(int p, int q) {
+    int depth = getDepth(p, q);
+    int breadth = getBreadth(p, q, depth);
+    int pos = 1 ;
 
+    for (int i = 0; i < depth; i++) {
+      pos += Math.pow(2, i);
+    }
+
+    pos += breadth;
+
+    return pos;
+  }
+
+ /*
+  * Method to get the depth of pair p/q in a complete binary tree. Traverse up
+  * the tree until we get to the root, where p = q = 1.
+  *
+  * @param int p
+  * @param int q
+  * @return int depth
+  */
+  private static int getDepth(int p, int q) {
+    int depth = 0;
+    double x = p;
+    double y = q;
+
+    while (x / y != 1) {
+      if (x / y < 1) {
+        y = (y - x);
+      } else if (x / y > 1) {
+        x = (x - y);
+      }
+
+      depth++;
+    }
+
+    return depth;
+  }
+
+  private static int getBreadth(int p, int q, int depth) {
+    int breadth = 0;
+    double x = p;
+    double y = q;
+
+    for (int i = 0; i < depth; i++) {
+      if (x / y < 1) {
+        y = (y - x);
+      } else if (x / y > 1) {
+        x = (x - y);
+        breadth += (Math.pow(2, i));
+      }
+    }
+
+    return breadth;
   }
 }
