47defdc1e0e7b2248bc04dceacc3c8543ae9980b
mh-o <michael.okonski@gmail.com>
2019-04-28 13:53:01 -0400

    setup problem D

---
+++ Google's Kick Start 2013/Round A/Problem D/CrossTheMaze.java
@@ -0,0 +1,25 @@
+/*
+ * Compile: `javac Sorting.java`
+ * Execute: `java Sorting < infile > outfile`
+ * Execute (powershell):
+ * > Get-Content infile | java Sorting | Out-File -Filepath outfile -Encoding ASCII
+ */
+
+ import java.util.*;
+ import java.io.*;
+
+ /*
+  * Class for the Cross the Maze problem from round A of Google's 2013 Kick
+  * Start coding challenge.
+  */
+public class CrossTheMaze {
+  static Scanner in;
+
+  public static void main(String[] args) {
+    in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+    int num_test_cases = in.nextInt();
+    in.nextLine();
+
+    
+  }
