9ce8d94a0782a703db9c0ca6ad0707fd37f86036
mh-o <michael.okonski@gmail.com>
2019-06-11 12:36:35 -0400

    add web design

---
+++ Undergrad CS334 Intermediate Web Design/Project 2/Project-Part-2-master/JS/js_proj1.js
@@ -0,0 +1,96 @@
+document.getElementById("homeButton").onclick = function () {
+  location.href = "root_proj1.html";
+};
+	
+document.getElementById("productsButton").onclick = function () {
+  location.href = "products_proj1.html";
+};
+	
+document.getElementById("cartButton").onclick = function ()
+{
+  location.href = "cart_proj1.html";
+};
+	
+document.getElementById("addButton1").onclick = function ()
+{
+  if (document.getElementById("addButton1").innerHTML == 'ADD')
+  {
+    document.getElementById("added1").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton1").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton1").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added1").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton1").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton2").onclick = function ()
+{
+  if (document.getElementById("addButton2").innerHTML == 'ADD')
+  {
+    document.getElementById("added2").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton2").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton2").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added2").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton2").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton3").onclick = function ()
+{
+  if (document.getElementById("addButton3").innerHTML == 'ADD')
+  {
+    document.getElementById("added3").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton3").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton3").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added3").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton3").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton4").onclick = function ()
+{
+  if (document.getElementById("addButton4").innerHTML == 'ADD')
+  {
+    document.getElementById("added4").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton4").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton4").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added4").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton4").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton5").onclick = function ()
+{
+  if (document.getElementById("addButton5").innerHTML == 'ADD')
+  {
+    document.getElementById("added5").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton5").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton5").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added5").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton5").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton6").onclick = function ()
+{
+  if (document.getElementById("addButton6").innerHTML == 'ADD')
+  {
+    document.getElementById("added6").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton6").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton6").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added6").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton6").innerHTML = 'ADD';
+  }
+};
---
+++ Undergrad CS334 Intermediate Web Design/Project 2/Project-Part-2-master/readme.txt
@@ -0,0 +1,3 @@
+I spent a large amount of time implimenting cookies into my shopping cart system,
+unfortunately in the end something got messed and my website lost the ability to
+store variables between pages, and the code just jumbled my site so I removed it.
